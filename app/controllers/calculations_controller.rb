2637f7913cb9579a71b59574e506b4c092c04de0
santiagolv <santiagolv@users.noreply.github.com>
2016-10-17 09:58:49 -0500

    word calc ok

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,19 +9,24 @@ class CalculationsController < ApplicationController
     # The text the user input is in the string @text.
     # The special word the user input is in the string @special_word.
     # ================================================================================
+    text_splitted = @text.split
+    text_splitted_length = text_splitted.length
+    counter = 0
+    special_word_occurrences = 0
+    while counter < text_splitted_length do
+      if text_splitted[counter]==@special_word || text_splitted[counter]==@special_word+";"|| text_splitted[counter]==@special_word+":"|| text_splitted[counter]==@special_word+"!"|| text_splitted[counter]==@special_word+"?"|| text_splitted[counter]==@special_word+"."|| text_splitted[counter]==@special_word+","
+        counter += 1
+        special_word_occurrences += 1
+          else
+        counter += 1
+      end
+    end
+    text_chomped = @text.gsub("\n"," ")
 
-
-    @character_count_with_spaces = "Replace this string with your answer."
-
-    @character_count_without_spaces = "Replace this string with your answer."
-
-    @word_count = "Replace this string with your answer."
-
-    @occurrences = "Replace this string with your answer."
-
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+    @character_count_with_spaces = @text.chomp.length
+    @character_count_without_spaces = text_chomped.squish.gsub(" ","").length
+    @word_count = @text.squish.count(" ")+1
+    @occurrences = special_word_occurrences
 
     render("word_count.html.erb")
   end
